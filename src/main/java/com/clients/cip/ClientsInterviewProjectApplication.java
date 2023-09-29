27963e49c9fcdc43c73d05366779287ad4ca3c78
Kalinin Alexey <leshakalinin93@gmail.com>
2023-09-29 16:12:14 +0300

    initial commit

---
+++ src/main/java/com/clients/cip/ClientsInterviewProjectApplication.java
@@ -0,0 +1,13 @@
+package com.clients.cip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientsInterviewProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClientsInterviewProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/clients/cip/ClientsInterviewProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clients.cip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientsInterviewProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
